69b6000d40e8726549ed507d8bd4b2629acb4490
contactharshavk <contactharshavk@gmail.com>
2018-08-10 19:12:23 +0530

    doc1

--- src/main/java/org/mindtree/practice/trainingprog/ApplicationMindtreeEmployee.java
+++ src/main/java/org/mindtree/practice/trainingprog/ApplicationMindtreeEmployee.java
@@ -3,14 +3,14 @@ package org.mindtree.practice.trainingprog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  *
  */
 
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
 @EnableCaching
 public class ApplicationMindtreeEmployee 
